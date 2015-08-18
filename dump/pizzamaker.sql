7993e38fdbe905f850ac4f8ff0098f0c9503971b
fariou73 <svajblhc3c>
2015-08-19 02:33:36 +0400

    update dump

--- dump/pizzamaker.sql
+++ dump/pizzamaker.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Авг 13 2015 г., 04:04
+-- Время создания: Авг 19 2015 г., 02:32
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -27,18 +27,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `ingredient` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `count` int(11) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ingredientCount` int(11) DEFAULT NULL,
+  `ingredientsName` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Дамп данных таблицы `ingredient`
 --
 
-INSERT INTO `ingredient` (`id`, `name`, `count`) VALUES
-(1, 'Sausage', 9);
+INSERT INTO `ingredient` (`id`, `ingredientCount`, `ingredientsName`) VALUES
+(5, 93, 'Pepper'),
+(6, 39, 'Sausage');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
